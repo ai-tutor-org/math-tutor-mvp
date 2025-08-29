d6b63b198719300f341439bac88995cf360b0861
Aniruddh Dubey <justani98@gmail.com>
2025-08-29 18:12:08 +0530

    Remove conflicting measurement

--- src/components/presentations/01-introduction/index.js
+++ src/components/presentations/01-introduction/index.js
@@ -1,5 +1,4 @@
 export { default as RoomIllustration } from './RoomIllustration';
 export { default as StandardUnits } from './StandardUnits';
-export { default as ConflictingMeasurements } from './ConflictingMeasurements';
 export { default as RulerMeasurement } from './RulerMeasurement';
 export { default as MeterStick } from './MeterStick';
